595cf070ca7ce37b23c0f26e6572e48fa6034758
East Agile <eastagile@East-Agile-5391.local>
2016-11-11 16:58:06 +0700

    [#134135305] - Write tests and implement search for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,10 @@
 class BooksController < ApplicationController
   def index
-    @selected_category = Category.find(params[:cat_id])
-    @books = @selected_category&.books
+    if params[:cat_id]
+      @selected_category = Category.find(params[:cat_id])
+      @books = @selected_category&.books
+    elsif params[:search]
+      @books = Book.search_by_book_info(params[:search])
+    end
   end
 end
