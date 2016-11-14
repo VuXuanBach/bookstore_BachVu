f208d90b4d8e8f2ce05ea081104b769c4690736b
East Agile <eastagile@East-Agile-5391.local>
2016-11-14 16:26:11 +0700

    [#134135305] - [r] - Add rspec tests for search book model

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -11,4 +11,18 @@ RSpec.describe Book, type: :model do
   it { is_expected.to validate_numericality_of(:unit_price).is_greater_than_or_equal_to(0) }
   it { is_expected.to validate_numericality_of(:total_rating_value).is_greater_than_or_equal_to(0) }
   it { is_expected.to validate_numericality_of(:total_rating_count).is_greater_than_or_equal_to(0) }
+
+  describe 'search books' do
+    before { create(:book, title: 'some thing') }
+
+    it 'has result when search books with correct query' do
+      books = Book.search_by_book_info('some')
+      expect(books).not_to be_empty
+    end
+
+    it 'does not have result when search books with correct query' do
+      books = Book.search_by_book_info('asd')
+      expect(books).to be_empty
+    end
+  end
 end
